76645e66329c717c3c1e7925629c9e346aecdc4b
codeWithAimeric <aimericjjb@gmail.com>
2024-09-13 14:28:33 +0300

    make resolvers

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "../schema";
+import { typeDefs } from "./schema";
+import db from "./_db";
 
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    authors() {
+      return db.authors
+    },
+    reviews() {
+      return db.reviews
+    }
+  }
+}
 // server setup
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers
 })
 
 
